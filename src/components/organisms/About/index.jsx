e68c47dbd28e19d66e2981653d90f8b53810915e
Joe Pena <joempena26@gmail.com>
2020-09-15 22:08:26 -0600

    Update Bio

--- src/components/organisms/About/index.jsx
+++ src/components/organisms/About/index.jsx
@@ -6,25 +6,29 @@ const About = () => (
     <h1>ABOUT JOE</h1>
     <p>
       Although I was born in the United States, I spent the majority of my life
-      in northen Mexico. At age 15, I decided to move back to the states, where
-      I live now. <br />
+      in Northen Mexico, in Chihuahua. At age 15, I decided to move back to the
+      states, just to look around and see how everything is over here, and what
+      do you know, I liked it enough that I stayed! <br />
       <br />
-      I've been a big fan of technology since a young age, and truly believe it
-      is what will change our lives in the near coming future, and so I'm happy
-      to be able to work in a field that allows for the innovation of
-      technology.
-      <br />
-      <br />
-      I'm very passionate about learning new methods and technologies in the
-      field, and always look forward to collaborating with others to find
-      effective and elegant solutions to tough problems.
+      I've been a big fan of technology since a young age, it started with me
+      playing games on an old Super NES and thinking "Wow, I can move what's on
+      the TV", then througout the years I saw the improvements on the same
+      technology that was showing me Mario on the screen, and it blew my mind
+      right out of my face.
       <br />
       <br />
-      During my free time, I enjoy exploring the great fourteeners of colorado,
-      shooting arrows in the archery range, and performing music as a hobby, as
-      well as collecting a great amount of classics from the 60s to the 80s,
-      with vynils from iconic names such as David Bowie, The Rolling Stones, and
-      Hall & Oates.
+      Come today, and it's amazing to think about exactly how fast technology
+      has progressed in just a decade alone, and I wanted to be a part of that
+      progress, so here I am, as a Software Engineer, doing what I can, getting
+      together with other engineers and building great things with just a
+      computer, it's crazy!
+      <br />
+      <br />
+      During my free time, I enjoy exploring the mountains of Colorado, shooting
+      arrows in the archery range, and performing music as a hobby, as well as
+      collecting a great amount of classics from the 60s to the 80s, with vynils
+      from iconic names such as David Bowie, The Rolling Stones, and Hall &
+      Oates.
     </p>
   </div>
 );
