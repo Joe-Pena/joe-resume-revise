dcefe9f2beb0532ad3a420c55203d029fd2e77a3
Joe Pena <joempena26@gmail.com>
2020-09-18 12:38:54 -0600

    Close menu on link click

--- src/components/organisms/Menu/index.jsx
+++ src/components/organisms/Menu/index.jsx
@@ -6,13 +6,14 @@ import StyledMenu from './Menu.styled';
 const Menu = ({ open, setOpen }) => {
   return (
     <StyledMenu open={open}>
-      <Link className="nav-link" href="#about">
+      <Link className="nav-link" href="#about" onClick={() => setOpen(!open)}>
         ABOUT
       </Link>
       <Link
         className="nav-link"
         target="_blank"
         href="https://www.linkedin.com/in/joe-pena/"
+        onClick={() => setOpen(!open)}
       >
         LINKEDIN
       </Link>
@@ -20,13 +21,23 @@ const Menu = ({ open, setOpen }) => {
         className="nav-link"
         target="_blank"
         href="https://github.com/Joe-Pena"
+        onClick={() => setOpen(!open)}
       >
         GITHUB
       </Link>
-      <Link className="nav-link" href="#projects">
+      <Link
+        className="nav-link"
+        href="#projects"
+        onClick={() => setOpen(!open)}
+      >
         PROJECTS
       </Link>
-      <Link className="nav-link" target="_blank" href={Resume}>
+      <Link
+        className="nav-link"
+        target="_blank"
+        href={Resume}
+        onClick={() => setOpen(!open)}
+      >
         RESUME
       </Link>
     </StyledMenu>
